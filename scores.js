963e4403c29ef79a884410561c35153568bb7512
Alek Bacon <kendalika3@gmail.com>
2023-03-06 17:52:58 -0700

    Adding file

---
+++ scores.js
@@ -0,0 +1,9 @@
+function loadScores() {
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+      scores = JSON.parse(scoresText);
+    }
+  
+    const tableBodyEl = document.querySelector('#scores');
+}
